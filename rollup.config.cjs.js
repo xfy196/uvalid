6fd3f8857e83fbc4764e183a45f2ec548ec24be3
xfy196 <xufy@tianji.cn>
2021-11-16 16:41:23 +0800

    加入esm,cjs,umd打包模式

--- rollup.config.cjs.js
+++ rollup.config.cjs.js
@@ -2,4 +2,5 @@ import rollupConfig from "./rollup.config"
 export default {...rollupConfig, output: {
     ...rollupConfig.output,
     format: "cjs",
+    file: "./dist/uvalid.cjs.js"
 }}
--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -1,6 +1,7 @@
 import rollupConfig from "./rollup.config"
 export default {...rollupConfig, output: {
     ...rollupConfig.output,
-    format: "esm",
-    file: "./dist/uvalid.esm.js"
+    format: "umd",
+    file: "./dist/uvalid.umd.js",
+    name: "uvalid.umd.js"
 }}
